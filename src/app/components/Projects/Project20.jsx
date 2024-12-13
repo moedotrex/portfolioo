3a369e51710543f0a039954e2196b65ae40a174c
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-12 23:35:23 -0500

    Update Project20.jsx

--- src/app/components/Projects/Project20.jsx
+++ src/app/components/Projects/Project20.jsx
@@ -29,13 +29,13 @@ const Project20 = () => {
           <p className="text-lg text-gray-400 mb-4">
           test text
           </p>
-        </div>
-        <div>
-            <video width="100%" height="auto" controls>
+          <video width="100%" height="auto" controls>
               <source src="/images/maCutscene.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
             <p className="text-md text-gray-500 mt-2">Final Animatic</p>
+        </div>
+        <div>
         </div>
         <div className="md:w-1/2 md:ml-8">
           <p className="text-xl text-gray-400 font-bold mb-2">Rigging and Animations</p>
