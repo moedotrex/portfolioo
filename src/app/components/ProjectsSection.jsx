a8c846419a5c7e16c800484990665dcff4fca86a
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-12 20:10:33 -0500

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -180,7 +180,7 @@ const projectsData = [
       id: 19,
       title: "Hottie's Cream",
       description: "Ice cream scooping simulator.",
-      image: "/images/projects/191.GIF",
+      image: "/images/projects/1.GIF",
       tag: ["All", "Games"],
       previewUrl: "/Project19",
     },
