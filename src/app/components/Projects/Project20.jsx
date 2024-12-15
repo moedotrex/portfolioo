e91d1b9655d487f130fd92b1d57b397d801ac031
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 18:17:13 -0500

    Update Project20.jsx

--- src/app/components/Projects/Project20.jsx
+++ src/app/components/Projects/Project20.jsx
@@ -31,6 +31,14 @@ const Project20 = () => {
           <p className="text-lg text-gray-400 mb-4">
           I rigged the monkey model in Blender, and then animated it within Unity, alongside other 3D assets, to develop an emotionally impactful cutscene that enriches the player&apos;s experience.
           </p>
+        <div className="mb-4">
+        <Image src="/images/masketches.PNG" alt="masketches" width={700} height={500} />
+        </div>
+        <p className="text-md text-gray-500 mt-2">First monkey sketches</p>
+        <div className="mb-4">
+        <Image src="/images/mamonkeyver2.png" alt="mamonkeyver2" width={700} height={500} />
+        </div>
+        <p className="text-md text-gray-500 mt-2">Early monkey textures</p>
         </div>
         <div>
         </div>
@@ -39,34 +47,40 @@ const Project20 = () => {
           <p className="text-lg text-gray-400 mb-4">
           Monkeying Around is a VR game that puts players in the shoes of a monkey toy, separated from its mother in the basement of a toy store. Players must navigate their way back to her by climbing between shelf levels, utilizing vertical movement to overcome various obstacles. The primary mechanic revolves around the rhythmic movement of the player&apos;s hands, which guides them through the environment. An emotional narrative unfolds through an animated cutscene, allowing players to develop a deeper emotional connection with the character.
           </p>
-          <div className="mb-4 mt-5"style={{ display: 'flex', gap: '10px' }}>
+          <div className="mb-4 mt-5"style={{ display: 'flex', gap: '5px' }}>
             <Image src="/images/mass1.png" alt="mass1" width={700} height={500} />
           </div>
-          <div className="mb-4 mt-5"style={{ display: 'flex', gap: '10px' }}>
+          <div className="mb-4 mt-5"style={{ display: 'flex', gap: '5px' }}>
             <Image src="/images/mass2.png" alt="mass2" width={700} height={500} />
           </div>
         <div className="mb-4"style={{ display: 'flex', gap: '5px' }}>
             <Image src="/images/mass4.png" alt="mass4" width={350} height={450} />
             <Image src="/images/mass5.png" alt="mass5" width={350} height={450} />
         </div>
-        <div className="mb-4"style={{ display: 'flex', gap: '5px' }}>
+        <p className="text-md text-gray-500 mt-2">Scene screenshots inside Unity</p>
+        <div className="mb-4"style={{ display: 'flex', gap: '10px' }}>
             <Image src="/images/mamonkeyturnaround.gif" alt="mamonkeyta" width={350} height={450} />
             <Image src="/images/mamouseturnaround.gif" alt="mamouseta" width={350} height={450} />
         </div>
+        <p className="text-md text-gray-500 mt-2">3D models with painterly textures</p>
           <div className="mb-4 mt-5"style={{ display: 'flex', gap: '10px' }}>
             <Image src="/images/mamonkeyver3.png" alt="mamonkeyver3" width={700} height={500} />
           </div>
           <div className="mb-4 mt-5"style={{ display: 'flex', gap: '10px' }}>
           <Image src="/images/mamousetexts.png" alt="mamousetexts" width={700} height={500} />
           </div>
-        <div className="mb-4">
-        <Image src="/images/maa2.png" alt="maa2" width={600} height={400} />
-        </div>
-        <p className="text-xl text-gray-400 font-bold mb-2 mt-2">About the normal maps</p>
+          <p className="text-md text-gray-500 mt-2">Textures made in Substance Painter</p>
+          <p className="text-xl text-gray-400 font-bold mb-2 mt-2">About the normal maps</p>
           <p className="text-lg text-gray-400 mb-4">
           After modeling in Blender, I generated the object&apos;s normal map and used Procreate to manually color match and paint textures directly onto it. This texture was then imported back into Blender to create a tangent normal map for integration into Unity.
         </p>
-       
+        <div className="mb-4"style={{ display: 'flex', gap: '10px' }}>
+            <Image src="/images/manormal1.png" alt="manormal1" width={350} height={450} />
+            <Image src="/images/manormal2.PNG" alt="manormal2" width={350} height={450} />
+        </div>
+        <div className="mb-4">
+        <Image src="/images/manormal3.PNG" alt="manormal3" width={700} height={500} />
+        </div>
         </div>
       </div>
     </div>
