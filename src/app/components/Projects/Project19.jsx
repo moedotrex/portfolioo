5b5de9f82297d099dff268dd21ccd4559a3cad1f
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 14:25:27 -0500

    Update Project19.jsx

--- src/app/components/Projects/Project19.jsx
+++ src/app/components/Projects/Project19.jsx
@@ -10,36 +10,40 @@ const Project19 = () => {
       <div className="mb-8 flex flex-col md:flex-row items-start">
         <div className="md:w-1/2">
           <div className="mb-4">
-            <Image src="/images/projects/19.gif" alt="icmain" width={610} height={410} />
+            <Image src="/images/icecreamgameplay.gif" alt="icmain" width={610} height={410} />
           </div>
           <p className="text-md text-gray-500 mb-4">
-            Video game created in 2 weeks for the Building Virtual Worlds class in CMU.
+           Gameplay
           </p>
           <p className="text-xl text-gray-400 font-bold mb-2">The Controls</p>
           <p className="text-lg text-gray-400 mb-4">
-          We created a physical interface for this game using Xbox Adaptive Controllers. We wanted to simulate a real life scooping ice cream experience.
+          Hottie's Cream stands out as an innovative experience by integrating a custom Xbox Adaptive Controller interface that redefines the typical gaming experience through tactile interaction. Unlike traditional games that rely solely on buttons and joysticks, this simulation allows players to engage in realistic ice cream scooping motions, providing a genuinely immersive experience. The unique control scheme caters to various accessibility needs, making it possible for a wider audience to enjoy gameplay that emphasizes natural movements and interaction. Additionally, the requirement for varying levels of eye contact to please characters introduces a distinctive social dynamic that could not be replicated with conventional controls, ensuring that players develop a deeper emotional connection with both the gameplay and the characters they serve.
           </p>
           <div className="mb-4 mt-5"style={{ display: 'flex', gap: '10px' }}>
           <Image src="/images/icecreammodels.png" alt="icm" width={600} height={400} />
           </div>
           <p className="text-xl text-gray-400 font-bold mb-2">Art</p>
           <p className="text-lg text-gray-400 mb-4">
-          test text
+          In this project, I served as an artist, contributing to the concept art for characters and the user interface. I created 3D models with custom textures using Substance Painter, and developed toon and glass shaders to elevate the visuals of the scene. Once the models were completed, I integrated everything into Unity. Additionally, I assisted with the background line art and contributed to the coloring and shading of several characters, helping to bring the vibrant world of Hottie's Cream to life.
           </p>
         </div>
         <div className="md:w-1/2 md:ml-8">
           <p className="text-xl text-gray-400 font-bold mb-2">About</p>
           <p className="text-lg text-gray-400 mb-4">
-          test text
+          Hottie's Cream is an immersive ice cream shop simulation game designed with a custom Xbox Adaptive Controller interface. Players can experience the delightful motions of scooping ice cream through a natural and intuitive physical setup. Interact with a diverse cast of characters from various fictional cities, each with their own unique needs for eye contact to ensure their satisfaction as customers. Strive to become employee of the month while navigating the challenges of keeping the company’s reputation intact.
           </p>
         <div className="mb-4">
         <Image src="/images/icecreamjarmodel.png" alt="icjm" width={600} height={400} />
         <Image src="/images/icecreamjargraph.png" alt="icjg" width={600} height={400} />
         <Image src="/images/icecreamtoongraph.png" alt="ictg" width={600} height={400} />
         </div>
-        <p className="text-xl text-gray-400 font-bold mb-2 mt-2">3D Models</p>
+        <p className="text-xl text-gray-400 font-bold mb-2 mt-2">The Team</p>
           <p className="text-lg text-gray-400 mb-4">
-          test text
+          This project was created for Round 1 of the Building Virtual Worlds class in Carnegie Mellon’s Master of Entertainment Technology program. It was developed in two weeks by a team of four.
+          *Art by Moe Aguilar and Skye Gao
+          *Programming by Yifan Jiang
+          *Sound design by Yuhuai Huang
+
           </p>
         </div>
       </div>
