874469d15d28b4b39c76ac2d87e2a0d1c706e96d
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 21:48:34 -0500

    Update VFXReel.jsx

--- src/app/components/VFXReel.jsx
+++ src/app/components/VFXReel.jsx
@@ -5,15 +5,13 @@ import Link from 'next/link';
 
 const VFXReel = () => {
   return (
-    <section className=" mt-20 mb-15">
-      <div className='grid grid-cols-1 sm:grid-cols-12'>
+    <section className=" mt-10 mb-15">
         <div className='col-span-7 place-self-center text-center'>
           <h1 className="text-white mb-6 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             Technical Art (VFX) Reel
           </h1>
         </div>
-      </div>
-      <div className="mb-4">
+      <div className="mt-4">
         <video width="70%" height="auto" controls>
               <source src="/images/moevfxreel.mp4" type="video/mp4" />
               Your browser does not support the video tag.
