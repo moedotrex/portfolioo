4c23866912ec0008e214b728295d33fd938bf854
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 21:28:43 -0500

    Update VFXReel.jsx

--- src/app/components/VFXReel.jsx
+++ src/app/components/VFXReel.jsx
@@ -8,9 +8,9 @@ const VFXReel = () => {
     <section className=" mt-20 mb-15">
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center'>
-          <h1 className="text-white mb-6 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            Technical Art (VFX) Reel
-          </h1>
+          <h2 className="text-center text-4xl font-bold text-white mt-2 mb-1 md:mb-1">
+          Technical Art (VFX) Reel
+          </h2>
         </div>
       </div>
       <div className="mb-4">
