5cbc1f423593143609e0119a94905b72e8397414
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 19:17:32 -0500

    Update Project21.jsx

--- src/app/components/Projects/Project21.jsx
+++ src/app/components/Projects/Project21.jsx
@@ -21,11 +21,26 @@ const Project21 = () => {
           <p className="text-xl text-gray-400 font-bold mb-2">Controls</p>
           <p className="text-lg text-gray-400 mb-4">
           This game was created for the Jam O Drum, a platform developed at Carnegie Mellon University that brings a unique social dimension to gaming. This innovative setup encourages players to collaborate and compete in a lively, interactive environment, enhancing the game&apos;s fun factor.
-
           </p>
           <div className="mb-4">
         <Image src="/images/fishswoosh.png" alt="fishswoosh" width={700} height={500} />
         </div>
+        <div className="mb-4">
+        <Image src="/images/fishsplashgraph.png" alt="fishsplashgraph" width={700} height={500} />
+        </div>
+        <div className="mb-4">
+        <Image src="/images/fishtexts.png" alt="fishtexts" width={700} height={500} />
+        </div>
+        <p className="text-md text-gray-500 mt-2">Shaders graphs made in Unity with custom Textures</p>
+        <p className="text-xl text-gray-400 font-bold mb-2 mt-2">The Team</p>
+          <p className="text-lg text-gray-400 mb-4">
+          This project was created for Round 3 of the Building Virtual Worlds class in Carnegie Mellon&apos;s Master of Entertainment Technology program. It was developed in one week by a team of five.
+          </p>
+          <div className="text-lg text-gray-400 mb-4">
+                <li>Programming by Moe Aguilar and Jerry Zheng</li>
+                <li>Art by Eliana Huang and Victor Jhou</li>
+                <li>Sound design by Jialan Dong</li>
+          </div>
         </div>
         <div className="md:w-1/2 md:ml-8">
           <p className="text-xl text-gray-400 font-bold mb-2">About</p>
@@ -44,6 +59,10 @@ const Project21 = () => {
           <p className="text-lg text-gray-400 mb-4">
           In this project, I took on the role of a programmer, helping out with the initial game scripts and managing some gameplay mechanics. However, I really focused on being a technical artist and spent a lot of my time creating cool visual effects (VFX) to amp up the visuals. I used Unity's Shader Graph and particle system, along with some custom textures, to add eye-catching visual feedback for the key player actions, making the gameplay experience even more engaging.
           </p>
+          <div className="mb-4">
+        <Image src="/images/fishassets.png" alt="fishassets" width={700} height={500} />
+        </div>
+        <p className="text-md text-gray-500 mt-2">Close up of the 3D assets made by Eliana and Victor</p>
         </div>
       </div>
     </div>
