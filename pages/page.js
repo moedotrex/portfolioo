d2ebc2890231903ccfdf5502e0bfb50130368991
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 20:51:34 -0500

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -8,10 +8,6 @@ import AboutSection from '../src/app/components/AboutSection';
 import ProjectsSection from '../src/app/components/ProjectsSection';
 import Footer from '../src/app/components/Footer';
 import EmailSection from '../src/app/components/EmailSection';
-import Project1 from '../src/app/components/Projects/Project1';
-import Project2 from '../src/app/components/Projects/Project2';
-import Project19 from '../src/app/components/Projects/Project19';
-import Project19Page from './Project19';
 // ... (import other projects)
 
 var stripe_load = () => {
