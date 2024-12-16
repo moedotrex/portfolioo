2a1f03e2319bf87d49571dbdcda73eb49b13df2d
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 20:34:18 -0500

    wotb update

--- src/app/components/Projects/Project23.jsx
+++ src/app/components/Projects/Project23.jsx
@@ -24,10 +24,17 @@ const Project23 = () => {
           Guests have the opportunity to engage with the flowers, discussing these elements of the table setting. The flowers include the Leatherflower (Clematis viorna) for Depression, the Southern Small Yellow Lady's-slipper (Cypripedium parviflorum var. parviflorum) for Anger, and the White Trout Lily (Erythronium albidum) for Denial. Together, they create an atmosphere rich with emotion, inviting reflection on the themes of loss and the importance of connection in the face of extinction.
 
           </p>
-
+          <div className="mb-4">
+        <Image src="/images/wotbsketches.PNG" alt="wotbsketches" width={700} height={500} />
+        </div>
+        <p className="text-md text-gray-500 mt-2">Early concept sketches</p>
+        <div className="mb-4">
+        <Image src="/images/wotbrig.png" alt="wotbrig" width={700} height={500} />
+        </div>
+        <p className="text-md text-gray-500 mt-2">Flower rig made in Blender</p>
           <p className="text-xl text-gray-400 font-bold mb-2">Contributions</p>
           <p className="text-lg text-gray-400 mb-4">
-          I was responsible for crafting the story, which draws inspiration from a short story I wrote a few years ago about flowers having feelings. In addition to the narrative, I created props for the physical tea party table setting and contributed to the art for the game&apos;s virtual elements. I also made a rig for the main flower and developed its animations to express its emotions.
+          I was responsible for crafting the story, which draws inspiration from a short story I wrote a few years ago about flowers having feelings. In addition to the narrative, I created props for the physical tea party table setting and contributed to the art for the game&apos;s virtual elements. I also made a rig for the main flower, added animations to express its emotions and some FVX for better visual feedback.
           </p>
         </div>
         <div className="md:w-1/2 md:ml-8">
@@ -43,10 +50,20 @@ const Project23 = () => {
             <Image src="/images/wotbno.gif" alt="wotbno" width={350} height={450} />
             <Image src="/images/wotbsadno.gif" alt="wotbsadno" width={350} height={450} />
         </div>
+        <p className="text-md text-gray-500 mt-2">Flower Animations</p>
+        <div className="mb-2"style={{ display: 'flex', gap: '5px' }}>
+            <Image src="/images/wotbsparkles.gif" alt="wotbsparkles" width={350} height={450} />
+            <Image src="/images/wotbquestion.gif" alt="wotbquestion" width={350} height={450} />
+        </div>
+        <p className="text-md text-gray-500 mt-2">Flower VFX</p>
+
         <p className="text-xl text-gray-400 font-bold mb-2 mt-2">Physical Setting</p>
           <p className="text-lg text-gray-400 mb-4">
           To fully engage the guest&apos;s senses, we provided tea and snacks, corresponding to each flower&apos;s story. The table for the tea party was decorated to feel magical and floral.
           </p>
+          <div className="mb-4">
+        <Image src="/images/wotbtable.jpg" alt="wotbtable" width={700} height={500} />
+        </div>
         </div>
       </div>
     </div>
