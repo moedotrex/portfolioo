1426ae3a4a8832465cf43014794df4a140021d54
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 19:09:18 -0500

    Update Project21.jsx

--- src/app/components/Projects/Project21.jsx
+++ src/app/components/Projects/Project21.jsx
@@ -23,18 +23,22 @@ const Project21 = () => {
           This game was created for the Jam O Drum, a platform developed at Carnegie Mellon University that brings a unique social dimension to gaming. This innovative setup encourages players to collaborate and compete in a lively, interactive environment, enhancing the game&apos;s fun factor.
 
           </p>
-          <div className="mb-4 mt-5"style={{ display: 'flex', gap: '10px' }}>
-            <Image src="/images/outer2.PNG" alt="o2" width={300} height={200} />
-            <Image src="/images/outer1.PNG" alt="o1" width={300} height={200} />
-          </div>
+          <div className="mb-4">
+        <Image src="/images/fishswoosh.png" alt="fishswoosh" width={700} height={500} />
+        </div>
         </div>
         <div className="md:w-1/2 md:ml-8">
           <p className="text-xl text-gray-400 font-bold mb-2">About</p>
           <p className="text-lg text-gray-400 mb-4">
           Sakana Smash is a vibrant, sushi-themed arena where players take on the roles of quirky, animated fish. Set within a lively sushi restaurant, the experience combines playful art with intense, physics-driven gameplay. As players navigate the conveyor belt and grill hazards, they must push and dodge each other to avoid becoming the next sushi roll! The fast-paced, chaotic environment offers quick rounds filled with humor, strategy, and surprise, creating an unforgettable, “bite-sized” experience for players and viewers alike.
           </p>
-        <div className="mb-4">
-        <Image src="/images/outer0.PNG" alt="o0" width={600} height={400} />
+          <div className="mb-2"style={{ display: 'flex', gap: '5px' }}>
+            <Image src="/images/fishpunch.gif" alt="fishpunch" width={350} height={450} />
+            <Image src="/images/fishsplash.gif" alt="fishsplash" width={350} height={450} />
+        </div>
+        <div className="mb-2"style={{ display: 'flex', gap: '5px' }}>
+            <Image src="/images/fishcloud.gif" alt="fishcloud" width={350} height={450} />
+            <Image src="/images/fishsakura.gif" alt="fishsakura" width={350} height={450} />
         </div>
         <p className="text-xl text-gray-400 font-bold mb-2 mt-2">VFX</p>
           <p className="text-lg text-gray-400 mb-4">
