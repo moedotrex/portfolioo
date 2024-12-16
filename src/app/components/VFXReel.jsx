1ecfaa61e43bf91f3932557111ea4891e2d085d0
moerex <monse.aguilar.sanchez@gmail.com>
2024-12-15 21:14:20 -0500

    Update VFXReel.jsx

--- src/app/components/VFXReel.jsx
+++ src/app/components/VFXReel.jsx
@@ -9,6 +9,7 @@ const VFXReel = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className="text-white mb-6 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            TEST VFX REEL
           </h1>
         </div>
       </div>
