5b0b21f53a0d02c8fea0f0778d9bdaacaa949ec8
moerex <69995282+moedotrex@users.noreply.github.com>
2024-12-18 22:19:09 -0600

    Update VFXReel.jsx

--- src/app/components/VFXReel.jsx
+++ src/app/components/VFXReel.jsx
@@ -7,9 +7,9 @@ const VFXReel = () => {
   return (
     <section className=" mt-10 mb-15">
         <div className='col-span-7 place-self-center text-center'>
-          <h2 className="text-white mb-6 text-4xl sm:text-6xl lg:text-6xl font-bold">
+          <h1 className="text-white mb-6 text-4xl sm:text-6xl lg:text-6xl font-bold">
             Technical Art (VFX) Reel
-          </h2>
+          </h1>
         </div>
       <div className="justify-center items-center mt-4">
         <video width="70%" height="auto" controls>
