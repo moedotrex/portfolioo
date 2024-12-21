c524aeb833248ab73c930bd108d9e229668cca0d
moerex <69995282+moedotrex@users.noreply.github.com>
2024-12-20 20:39:47 -0600

    Update VFXReel.jsx

--- src/app/components/VFXReel.jsx
+++ src/app/components/VFXReel.jsx
@@ -9,13 +9,14 @@ const VFXReel = () => {
         <div className='col-span-7 place-self-center text-center'>
           <h1 className="text-white mb-6 text-4xl sm:text-6xl lg:text-6xl font-bold">
             Technical Art (VFX) Reel
-          </h1>
-        </div>
-      <div className="justify-center items-center mt-4">
-        <video width="70%" height="auto" controls>
+            <video width="70%" height="auto" controls>
               <source src="/images/moevfxreel.mp4" type="video/mp4" />
               Your browser does not support the video tag.
         </video>
+          </h1>
+        </div>
+      <div className="justify-center items-center mt-4">
+
       </div>
     </section>
   );
