fa0ef30d860578062997ceab2c918ea5a09cf3e7
moerex <69995282+moedotrex@users.noreply.github.com>
2026-01-09 18:01:55 -0500

    Update Project25.jsx

--- src/app/components/Projects/Project25.jsx
+++ src/app/components/Projects/Project25.jsx
@@ -22,18 +22,10 @@ const Project25 = () => {
           <p className="text-lg text-gray-400 mb-4">
           Scotty3D is a 3D graphics software package built during Carnegie Mellon's 15-462 Computer Graphics course. It includes components for software rastization, interactive mesh editing, realistic path tracing, and dynamic animation. Implementing functionality in each of these areas constitutes the majority of the coursework for the class.
           </p>
-          <div className="mb-4">
-        <Image src="/images/wotbsketches.PNG" alt="wotbsketches" width={700} height={500} />
-        </div>
-        <p className="text-md text-gray-500 mt-2 mb-3">Early concept sketches</p>
-        <p className="text-xl text-gray-400 font-bold mb-2">Other Contributions</p>
+        <p className="text-xl text-gray-400 font-bold mb-2">Reference</p>
           <p className="text-lg text-gray-400 mb-4">
-          I also made some of the 2D backgrounds, as well as concept sketches and storyboards for some of the game&apos;s cutscenes.
+          For reference: CMU Scotty3D Documentation
           </p>
-        <div className="mb-4">
-        <Image src="/images/wotbrig.png" alt="wotbrig" width={700} height={500} />
-        </div>
-        <p className="text-md text-gray-500 mt-2">Flower rig made in Blender</p>
         </div>
         <div className="md:w-1/2 md:ml-8">
           <p className="text-xl text-gray-400 font-bold mb-2">Features Implemented</p>
@@ -56,27 +48,6 @@ const Project25 = () => {
           <p className="text-md text-gray-500 mb-4">
           Animation made with Scotty3D
           </p>
-        <p className="text-xl text-gray-400 font-bold mb-2 mt-2">Physical Setting</p>
-          <p className="text-lg text-gray-400 mb-4">
-          To fully engage the guest&apos;s senses, we provided tea and snacks, corresponding to each flower&apos;s story. The table for the tea party was decorated to feel magical and floral.
-          </p>
-          <div className="mb-4">
-        <Image src="/images/wotbtable.jpg" alt="wotbtable" width={700} height={500} />
-        </div>
-        <p className="text-xl text-gray-400 font-bold mb-2 mt-2">The Team</p>
-          <p className="text-lg text-gray-400 mb-4">
-          This project was created for the Visual Story class in Carnegie Mellon&apos;s Master of Entertainment Technology program. It was developed in approximately two months by a team of six.
-          </p>
-          <div className="text-lg text-gray-400 mb-4">
-                <li>Art by Moe Aguilar, Harley Xu and Eunyoung Park</li>
-                <li>Programming by Ethan Zhu and Vector Liu</li>
-                <li>Story by Moe Aguilar and Joshua Kaplan</li>
-                <li>Props by Moe Aguilar and Joshua Kaplan</li>
-          </div>
-          <div className="mb-4">
-        <Image src="/images/wotb6lives.GIF" alt="wotb6lives" width={700} height={500} />
-        </div>
-        <p className="text-md text-gray-500 mt-2">Animated logo I made for our team</p>
         </div>
       </div>
     </div>
