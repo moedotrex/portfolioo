e5527f134fd3919c4c82ed649b1095da921d2f90
moerex <69995282+moedotrex@users.noreply.github.com>
2026-01-09 19:19:46 -0500

    updates

--- src/app/components/Projects/Project24.jsx
+++ src/app/components/Projects/Project24.jsx
@@ -21,6 +21,11 @@ const Project24 = () => {
           <p className="text-lg text-gray-400 mb-4">
           I also made some of the 2D backgrounds, as well as concept sketches and storyboards for some of the game&apos;s cutscenes.
           </p>
+          <p className="text-xl text-gray-400 font-bold mb-2">Awards and Recognition</p>
+          <p className="text-lg text-gray-400 mb-4">
+          This project was awarded Best Student-Developed Game in the 2025 Serious Games Showcase & Challenge (SGS&C) at I/ITSEC (Interservice / Industry Training, Simulation, and Education Conference) in Orlando, Florida.
+          It was also showcased in the Serious Play Conference 2025 in Rochester, New Yorl.
+          </p>
         <div className="mb-4">
         <Image src="/images/wotbrig.png" alt="wotbrig" width={700} height={500} />
         </div>
