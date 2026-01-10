ba751aea2ad1f751a3d6113b560d9a0cf9082475
moerex <69995282+moedotrex@users.noreply.github.com>
2026-01-10 17:16:50 -0500

    updates bjj

--- src/app/components/Projects/Project27.jsx
+++ src/app/components/Projects/Project27.jsx
@@ -35,7 +35,7 @@ const Project27 = () => {
         <div className="mb-2">
         <Image src="/images/q2ref.PNG" alt="q2ref" width={700} height={500} />
         </div>
-        <p className="text-md text-gray-500 mt-2 mb-3">Grafitti Pieces I made</p>
+        <p className="text-md text-gray-500 mt-2 mb-3">Grafitti pieces I made</p>
         <div className="mb-4">
         <Image src="/images/EndingBJJ.PNG" alt="EndingBJJ" width={700} height={500} />
         </div>
