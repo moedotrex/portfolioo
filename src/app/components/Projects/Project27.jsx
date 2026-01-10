377e02fee15b2e3515a4a0361084b22e3937dc03
moerex <69995282+moedotrex@users.noreply.github.com>
2026-01-10 17:55:31 -0500

    Update Project27.jsx

--- src/app/components/Projects/Project27.jsx
+++ src/app/components/Projects/Project27.jsx
@@ -30,8 +30,8 @@ const Project27 = () => {
         <div className="mb-0">
         <Image src="/images/q2ref.PNG" alt="q2ref" width={700} height={500} />
         </div>
-        <p className="text-md text-gray-500 mb-2">Grafitti pieces I made</p>
-        <div className="mb-4">
+        <p className="text-md text-gray-500 mb-4">Graffiti pieces I made</p>
+        <div className="mb-2">
         <Image src="/images/EndingBJJ.PNG" alt="EndingBJJ" width={700} height={500} />
         </div>
         <p className="text-md text-gray-500">Ending Illustration</p>
