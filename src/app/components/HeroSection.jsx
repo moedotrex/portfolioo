ef19402b43a564f95aa0d4968e16f43fb6da4bc4
moerex <69995282+moedotrex@users.noreply.github.com>
2026-01-11 18:39:39 -0500

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -43,8 +43,8 @@ const HeroSection = () => {
               </button>
             </Link>
             <a
-              href='/MoeAguilar-Resume2025.pdf' // Updated path assuming the CV is in the public directory
-              download={'MoeAguilar-CV2023.pdf'}
+              href='/MoeAguilar-Resume2026.pdf'
+              download={'MoeAguilar-Resume2026.pdf'}
             >
               <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
                 <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download Resume</span>
