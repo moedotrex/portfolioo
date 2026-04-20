0988fa68c334a58850791517bdc78beb751aa71e
moerex <69995282+moedotrex@users.noreply.github.com>
2026-04-20 11:42:08 -0400

    Update Narrative.jsx

--- src/app/components/Narrative.jsx
+++ src/app/components/Narrative.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Narrative = () => {
   return (
-    <section className="mt-10 mb-15">
+    <section className="mt-40 mb-20">
       <div className='col-span-7 place-self-center text-center'>
         <h1 className="text-white mb-6 text-4xl sm:text-6xl lg:text-6xl font-bold">
           Narrative Portfolio
