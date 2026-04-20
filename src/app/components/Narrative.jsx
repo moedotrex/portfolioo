2ebe7293a0028fdafdd8a94c526ff3f4fa430524
moerex <69995282+moedotrex@users.noreply.github.com>
2026-04-20 11:46:24 -0400

    updated narrative

--- src/app/components/Narrative.jsx
+++ src/app/components/Narrative.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Narrative = () => {
   return (
-    <section className="mt-40 mb-20">
+    <section className="mt-35 mb-20">
       <div className='col-span-7 place-self-center text-center'>
-        <h1 className="text-white mb-6 text-4xl sm:text-6xl lg:text-6xl font-bold">
+        <h1 className="text-white mb-10 text-4xl sm:text-6xl lg:text-6xl font-bold">
           Narrative Portfolio
         </h1>
       </div>
-      <div className="flex justify-center items-center mt-4 px-4">
+      <div className="flex justify-center items-center mt-8 px-4">
         <iframe 
           src="/images/MoeNarrativePortfolio.pdf"
           width="70%" 
